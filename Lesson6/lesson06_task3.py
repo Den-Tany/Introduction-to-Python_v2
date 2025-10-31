5733e8f5cbb33a069408ce8d938f6d5b7bdbed29
Den-Tany <77bobcat77@mail.ru>
2025-10-31 17:52:07 +0300

    lesson6_v2

--- Lesson6/lesson06_task3.py
+++ Lesson6/lesson06_task3.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -17,9 +19,12 @@ def select_browser(browser):
     elif browser == '2':
         return webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))       
 driver = select_browser(browser)
-driver.implicitly_wait(16)
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
+# driver.implicitly_wait(16)
+
+driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+element = WebDriverWait (driver, 20).until(
+    EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'p#text.lead'),'Done!'))
 waiting_picture = driver.find_element(By.CSS_SELECTOR, "img#award").get_attribute("src")
 
 print(waiting_picture)
