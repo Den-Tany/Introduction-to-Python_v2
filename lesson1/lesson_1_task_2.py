d07ffe844e3f5c8de6dca1b785970bd0258ecfff
Den-Tany <77bobcat77@mail.ru>
2025-11-23 00:07:59 +0300

    remov

---
+++ lesson1/lesson_1_task_2.py
@@ -0,0 +1,3 @@
+my_age=48 # Возраст
+my_age=my_age+3 # Возраст через 3 года
+print(my_age)
---
+++ lesson1/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+first_name=input("Ваше фамилия ")
+last_name=input("Ваше имя " )
+print("Вас зовут: " + first_name + " "+ last_name)
